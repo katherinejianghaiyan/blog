27acd52f05a3084e516891debaf9eab89e051595
angel.jiang <angel.jiang@adenservices.com>
2022-06-03 20:55:50 +0800

    日志管理

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = '3(blz*oezxke$_b#u!utux20h4f9-=5$=o#&us5$s)ug0l^r0v'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -69,18 +67,61 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'blog.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
+        'HOST': '127.0.0.1',
+        'PORT': '',
+        'USER': 'sa',
+        'PASSWORD': 'Adendb123',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
-
+LOGGING = {
+      'version': 1,
+      'disable_existing_loggers': False,
+      'formatters': { # 日志信息显示的格式
+          'verbose': {
+              'format': '%(levelname)s %(asctime)s %(module)s %(lineno)d %(message)s'
+          },
+          'simple': {
+              'format': '%(levelname)s %(module)s %(lineno)d %(message)s'
+          },
+      },
+      'filters':{ #对日志进行过滤
+          'require_debug_true': { # django在debug模式下才输出日志
+                '()': 'django.utils.log.RequireDebugTrue',
+          }
+      },
+      'handlers': {
+          'console': { # 向终端中输出日志
+                'level': 'INFO',
+                'filters': ['require_debug_true'],
+                'class': 'logging.StreamHandler',
+                'formatter':'simple'
+          },
+          'file': { # 向文件中输入日志
+                'level': 'INFO',
+                'class': 'logging.handlers.RotatingFileHandler',
+                'filename': os.path.join(BASE_DIR, 'logs/blog.log'), # 日志文件的位置
+                'maxBytes': 300 * 1024 * 1024,
+                'backupCount': 10,
+                'formatter': 'verbose'
+          },
+      },
+      # 日志器
+      'loggers': {
+         'django': {                        # 定义了一个名为django的日志器
+            'handlers':['console', 'file'], # 可以同时向终端和文件中输入日志
+            'propagate': True,              # 是否继续传递日志信息
+            'level': 'INFO'                 # 日志器接受的最低日志级别
+         }
+      }
+}
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
@@ -99,7 +140,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
@@ -112,7 +152,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
